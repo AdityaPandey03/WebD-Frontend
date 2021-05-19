7b443ce26fba9456379dbf0acc4a8b41c0aade83
Aditya Pandey <pandey.aditya4272@gmail.com>
2021-05-19 11:31:42 +0530

    updated notes+fetching noteId

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,12 +3,12 @@ const cardContainer=document.querySelector(".card-container");
 const cardData=
 [
     {heading: "heading1", content:"adiPandey", id:1 },
-    {heading: "heading1", content:"adiPandey", id:2 },
-    {heading: "heading1", content:"adiPandey", id:3 },
-    {heading: "heading1", content:"adiPandey", id:4 },
-    {heading: "heading1", content:"adiPandey", id:5 },
-    {heading: "heading1", content:"adiPandey", id:6 },
-    {heading: "heading1", content:"adiPandey", id:7 },
+    {heading: "heading2", content:"adiPandey", id:2 },
+    {heading: "heading3", content:"adiPandey", id:3 },
+    {heading: "heading4", content:"adiPandey", id:4 },
+    {heading: "heading5", content:"adiPandey", id:5 },
+    {heading: "heading6", content:"adiPandey", id:6 },
+    {heading: "heading7", content:"adiPandey", id:7 },
 ];
 
 const createNotes=((array)=>{
@@ -19,7 +19,7 @@ const createNotes=((array)=>{
         card.classList.add("card");
         card.id=id;
 
-        const insideHtml=`<div class="card-header"><div class="card-heading">${heading}</div><div class="edit-note"><img src="../../asset/edit_note.svg"></div></div><div class="card-content">${content}</div>`
+        const insideHtml=`<div class="card-header"><div class="card-heading">${heading}</div><a href="../UpdateNotes/updateNotes.html?noteId=${id}"><div class="edit-note"><img src="../../asset/edit_note.svg"></div></a></div><div class="card-content">${content}</div>`
 
     card.innerHTML= insideHtml;
 
